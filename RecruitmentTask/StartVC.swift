c0a166dfa21ac5276c288eb9e1b1a266c7cd5d7d
Yuliia Pavlenko <>
2020-04-02 20:18:53 +0200

    Fix Start vc

--- RecruitmentTask/StartVC.swift
+++ RecruitmentTask/StartVC.swift
@@ -7,30 +7,34 @@
 //
 
 import UIKit
-import PureLayout
 
 class StartVC: UIViewController {
 
-    let someImageView: UIImageView = {
-       let theImageView = UIImageView()
-       theImageView.image = UIImage(named: "applover.png")
-       theImageView.translatesAutoresizingMaskIntoConstraints = false
-       return theImageView
+    let logoImageView: UIImageView = {
+       let imageView = UIImageView()
+       imageView.image = UIImage(named: "applover.png")
+       imageView.translatesAutoresizingMaskIntoConstraints = false
+       return imageView
     }()
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
         
-        view.addSubview(someImageView)
+        view.backgroundColor = .white
+        view.addSubview(logoImageView)
         someImageViewConstraints()
     }
     
+    
     func someImageViewConstraints() {
         let widthHeight: CGFloat = 156.81
-        someImageView.widthAnchor.constraint(equalToConstant: widthHeight).isActive = true
-        someImageView.heightAnchor.constraint(equalToConstant: widthHeight).isActive = true
-        someImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        someImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80).isActive = true
+        logoImageView.widthAnchor.constraint(equalToConstant: widthHeight).isActive = true
+        logoImageView.heightAnchor.constraint(equalToConstant: widthHeight).isActive = true
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80).isActive = true
     }
 }
