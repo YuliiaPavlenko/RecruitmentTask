e8b209111afb90a45446ea2425532d92aa6cedb8
Yuliia Pavlenko <>
2020-04-02 18:37:51 +0200

    Add Start vc

--- RecruitmentTask/StartVC.swift
+++ RecruitmentTask/StartVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartVC.swift
 //  RecruitmentTask
 //
 //  Created by Yuliia Pavlenko on 02/04/2020.
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import PureLayout
 
 class StartVC: UIViewController {
 
+    let someImageView: UIImageView = {
+       let theImageView = UIImageView()
+       theImageView.image = UIImage(named: "applover.png")
+       theImageView.translatesAutoresizingMaskIntoConstraints = false
+       return theImageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        
+        view.addSubview(someImageView)
+        someImageViewConstraints()
     }
-
-
+    
+    func someImageViewConstraints() {
+        let widthHeight: CGFloat = 156.81
+        someImageView.widthAnchor.constraint(equalToConstant: widthHeight).isActive = true
+        someImageView.heightAnchor.constraint(equalToConstant: widthHeight).isActive = true
+        someImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        someImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80).isActive = true
+    }
 }
-
