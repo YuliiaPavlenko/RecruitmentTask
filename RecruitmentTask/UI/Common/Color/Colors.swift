9ca2c087fb8c9f0c01acb74e701a82535ad6310c
Yuliia Pavlenko <>
2020-04-02 16:48:28 +0200

    Add custom fonts and colors

---
+++ RecruitmentTask/UI/Common/Color/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  RecruitmentTask
+//
+//  Created by Yuliia Pavlenko on 02/04/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import UIKit
+
+class Colors {
+    static let green = UIColor(red: 0/255, green: 233/255, blue: 179/255, alpha: 1)
+    static let black = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1)
+    static let white = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    static let bodyText = UIColor(red: 116/255, green: 117/255, blue: 118/255, alpha: 1)
+    static let grayTitle = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.87)
+    static let graySubtitle = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.54)
+    static let separatorColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.24)
+}
