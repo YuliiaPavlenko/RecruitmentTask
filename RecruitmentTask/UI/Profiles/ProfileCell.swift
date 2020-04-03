e8c2bdfd3a6d7a4296fd23f4743528e600be0407
Yuliia Pavlenko <>
2020-04-03 22:42:59 +0200

    Add Profile cell UI

--- RecruitmentTask/UI/Profiles/ProfileCell.swift
+++ RecruitmentTask/UI/Profiles/ProfileCell.swift
@@ -10,4 +10,61 @@ import UIKit
 
 class ProfileCell: UITableViewCell {
      
+    public let userNameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.textColor = .black
+        nameLabel.font = Fonts.title
+        nameLabel.textAlignment = .left
+        return nameLabel
+    }()
+    
+    
+    public let userEmailLabel: UILabel = {
+        let emailLabel = UILabel()
+        emailLabel.textColor = Colors.graySubtitle
+        emailLabel.font = Fonts.subtitle
+        emailLabel.textAlignment = .left
+        emailLabel.numberOfLines = 0
+        return emailLabel
+    }()
+    
+    public let userPhoneLabel: UILabel = {
+        let phoneLabel = UILabel()
+        phoneLabel.textColor = Colors.graySubtitle
+        phoneLabel.font = Fonts.subtitle
+        phoneLabel.textAlignment = .left
+        phoneLabel.numberOfLines = 0
+        return phoneLabel
+    }()
+    
+    private let profileImage: UIImageView = {
+        var image = UIImage(named:"applover")
+        let imgView = UIImageView(image: image)
+        imgView.contentMode = .scaleAspectFit
+        imgView.clipsToBounds = true
+        return imgView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(userNameLabel)
+        addSubview(userEmailLabel)
+        addSubview(userPhoneLabel)
+        addSubview(profileImage)
+        
+        profileImage.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: nil, paddingTop: 12, paddingLeft: 26, paddingBottom: 12, paddingRight: 0, width: 40, height: 0, enableInsets: false)
+        userNameLabel.anchor(top: topAnchor, left: profileImage.rightAnchor, bottom: nil, right: nil, paddingTop: 11, paddingLeft: 19, paddingBottom: 0, paddingRight: 0, width: frame.size.width / 2, height: 19, enableInsets: false)
+        
+        let stackView = UIStackView(arrangedSubviews: [userEmailLabel, userPhoneLabel])
+        stackView.distribution = .equalSpacing
+        stackView.axis = .horizontal
+        stackView.spacing = 5
+        addSubview(stackView)
+        stackView.anchor(top: userNameLabel.bottomAnchor, left: profileImage.rightAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 19, paddingBottom: 0, paddingRight: 10, width: 0, height: 0, enableInsets: false)
+    
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- RecruitmentTask/UI/Profiles/ProfileViewController.swift
+++ RecruitmentTask/UI/Profiles/ProfileViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     let tableView = UITableView()
+    let cellId = "cellId"
+    let rowHeight: CGFloat = 64
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -20,6 +22,12 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.layoutMargins = UIEdgeInsets.zero
+        tableView.separatorInset = UIEdgeInsets.zero
+        tableView.separatorColor = Colors.separatorColor
+        tableView.register(ProfileCell.self, forCellReuseIdentifier: cellId)
     }
     
     func setupTableView() {
@@ -41,14 +49,25 @@ class ProfileViewController: UIViewController {
 
 
 // MARK: - UITableView Delegate & DataSource
-//extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return 1
-//    }
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        
-//    }
-//    
-//    
-//}
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! ProfileCell
+        cell.userNameLabel.text = "Test Name"
+        cell.userEmailLabel.text = "Test Email"
+        cell.userPhoneLabel.text = "1234567890"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return rowHeight
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
