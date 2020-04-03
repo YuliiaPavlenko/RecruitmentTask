7f8087be2a9dee00f6edf19940c055da246ea7b7
Yuliia Pavlenko <>
2020-04-03 18:10:38 +0200

    Add profile cell

---
+++ RecruitmentTask/UI/Profiles/ProfileCell.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileCell.swift
+//  RecruitmentTask
+//
+//  Created by Yuliia Pavlenko on 02/04/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+     
+}
